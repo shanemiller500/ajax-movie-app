a0cc067b44746f75d93a40747223e5d8d7b45a7a
shane <shanemiller500@gmail.com>
2014-06-11 11:05:46 -0700

    updated api call

--- script.js
+++ script.js
@@ -19,29 +19,34 @@ $(function() {
     var responce = $.ajax(request)
     responce.done(function(data) {
       $.each(data["Search"], function(index, movie) {
-       var info = $(".results").append("<li data-imdbid=\"" + movie["imbdID"] + "\">" + movie["Title"] + " " + movie["Year"] + "</li>" + "<br>")
-       console.log(info)
+        var info = $(".results").append("<li data-imdbid=\"" + movie["imdbID"] + "\">" + movie["Title"] + " " + movie["Year"] + "</li>" + "<br>")
+        // console.log(info)
 
       });
-    });
 
-    $(".results").delegate("li", "click", function(e) {
-      var imdata = $(e.target).data("data-imdbid")
-      console.log(imdata);
-      var request = {
-        url: "http://www.omdbapi.com/",
-        type: "get",
-        dataType:"json",
-        data: {
-          i: imdata
+
+      $(".results").delegate("li", "click", function(e) {
+        var imdata = $(e.target).data("imdbid")
+        var request = {
+          url: "http://www.omdbapi.com",
+          type: "get",
+          dataType: "json",
+          data: {
+            i: imdata
+          }
+
         }
-      }
+       
 
-      var responce2 = $.ajax(request)
-      responce2.done(function(data) {
-        $.each(data["data-imdbid"])
-        console.log(data)
-      })
+        var responce = $.ajax(request)
+        responce.done(function(data) {
+          $.each(data, function(img) {
+            var info = $(".image").append("<img src = data['Poster']" + "</img>" + "<li>" + data["Plot"] )
+             // $("<img>").attr("src", data["Poster"]).append(img)
+          })
+          console.log(data)
+        })
+      });
     });
   });
 });
