1581dc376ef7bb4f3d954a3c3d5692e8ab15245f
shane <shanemiller500@gmail.com>
2014-06-10 18:49:20 -0700

    BROKEN

--- script.js
+++ script.js
@@ -19,8 +19,29 @@ $(function() {
     var responce = $.ajax(request)
     responce.done(function(data) {
       $.each(data["Search"], function(index, movie) {
-        $(".results").append("<li>" + movie["Title"] + " " + movie["Year"] + "</li>")
+       var info = $(".results").append("<li data-imdbid=\"" + movie["imbdID"] + "\">" + movie["Title"] + " " + movie["Year"] + "</li>" + "<br>")
+       console.log(info)
+
+      });
+    });
+
+    $(".results").delegate("li", "click", function(e) {
+      var imdata = $(e.target).data("data-imdbid")
+      console.log(imdata);
+      var request = {
+        url: "http://www.omdbapi.com/",
+        type: "get",
+        dataType:"json",
+        data: {
+          i: imdata
+        }
+      }
+
+      var responce2 = $.ajax(request)
+      responce2.done(function(data) {
+        $.each(data["data-imdbid"])
+        console.log(data)
       })
-    })
+    });
   });
 });
